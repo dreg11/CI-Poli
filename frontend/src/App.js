39c08da9fff998c9746cee3f0288128a474c6219
dreg11 <gerodriguez20@poligran.edu.co>
2025-06-24 16:42:38 -0500

    docker funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
-
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTask, setNewTask] = useState('');
 
+  // URL del backend accesible desde Docker Compose
+  const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+;
+
   useEffect(() => {
-    fetch('http://localhost:3001/todos')
+    fetch(`${API_URL}/todos`)
       .then(res => res.json())
       .then(data => setTodos(data));
   }, []);
 
   const addTodo = () => {
-    fetch('http://localhost:3001/todos', {
+    fetch(`${API_URL}/todos`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ task: newTask }),
     })
       .then(() => {
         setNewTask('');
-        return fetch('http://localhost:3001/todos');
+        return fetch(`${API_URL}/todos`);
       })
       .then(res => res.json())
       .then(data => setTodos(data));
